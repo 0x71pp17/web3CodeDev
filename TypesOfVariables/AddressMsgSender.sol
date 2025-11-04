76bfb0b066f0b7c920d192a2e450ea1941e3efdd
0x71pp17 <tippitj@gmail.com>
2025-11-03 21:00:11 -0500

    Create AddressMsgSender.sol

---
+++ TypesOfVariables/AddressMsgSender.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+/// @title Addresses
+/// @author 0x71pp17
+/// @notice Demonstrates address manipulation and balance querying
+contract AddressMsgSender {
+    // Public state variable to store an Ethereum address
+    address public someAddress;
+
+    /// @notice Set the stored address to msg.sender address
+    function setSomeAddress() public {
+        someAddress = msg.sender;
+    }
+}   
