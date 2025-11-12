c53dfcefca326c35d31066e703b4a7a0e5dd58d9
0x71pp17 <tippitj@gmail.com>
2025-11-12 18:00:38 -0500

    Update TryCatchErrorHandling.sol

--- ExceptionHandling/TryCatchErrorHandling.sol
+++ ExceptionHandling/TryCatchErrorHandling.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-/// @title WillThrow
 /// @author 0x71pp17
 /// @notice A contract that demonstrates a function reverting with an error message
 contract WillThrow {
